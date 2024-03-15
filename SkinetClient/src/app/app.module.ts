dc65cbb3f6bc032ab880ccdd884e3985aa5f1319
viktorp999 <viktorpetrovski99@gmail.com>
2024-03-15 18:26:25 +0100

    Added navbar

--- SkinetClient/src/app/app.module.ts
+++ SkinetClient/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
