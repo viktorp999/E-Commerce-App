a2439edec242351b102e9a99865432431192174c
viktorp999 <viktorpetrovski99@gmail.com>
2024-03-20 00:03:20 +0100

    Make pagination working

--- SkinetClient/src/app/shop/shop.service.ts
+++ SkinetClient/src/app/shop/shop.service.ts
@@ -4,6 +4,7 @@ import { Pagination } from '../shared/models/pagination';
 import { Product } from '../shared/models/product';
 import { Brand } from '../shared/models/brand';
 import { Type } from '../shared/models/type';
+import { ShopParams } from '../shared/models/shopParams';
 
 @Injectable({
   providedIn: 'root',
@@ -12,20 +13,20 @@ export class ShopService {
   baseUrl = 'https://localhost:5001/api/';
   constructor(private http: HttpClient) {}
 
-  getProducts(brandId?: number, typeId?: number, sort?: string) {
+  getProducts(shopParams: ShopParams) {
     let params = new HttpParams();
 
-    if (brandId) {
-      params = params.append('brandId', brandId);
+    if (shopParams.bandId > 0) {
+      params = params.append('brandId', shopParams.bandId);
     }
 
-    if (typeId) {
-      params = params.append('typeId', typeId);
+    if (shopParams.typeId > 0) {
+      params = params.append('typeId', shopParams.typeId);
     }
 
-    if (sort) {
-      params = params.append('sort', sort);
-    }
+    params = params.append('sort', shopParams.sort);
+    params = params.append('pageIndex', shopParams.pageNumber);
+    params = params.append('pageSize', shopParams.pageSize);
 
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {
       params,
