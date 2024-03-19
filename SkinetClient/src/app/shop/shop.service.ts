e51b1c9d421a909de569068950e667a0e9fbc555
viktorp999 <viktorpetrovski99@gmail.com>
2024-03-19 23:03:51 +0100

    Sorting in client side

--- SkinetClient/src/app/shop/shop.service.ts
+++ SkinetClient/src/app/shop/shop.service.ts
@@ -12,7 +12,7 @@ export class ShopService {
   baseUrl = 'https://localhost:5001/api/';
   constructor(private http: HttpClient) {}
 
-  getProducts(brandId?: number, typeId?: number) {
+  getProducts(brandId?: number, typeId?: number, sort?: string) {
     let params = new HttpParams();
 
     if (brandId) {
@@ -22,6 +22,11 @@ export class ShopService {
     if (typeId) {
       params = params.append('typeId', typeId);
     }
+
+    if (sort) {
+      params = params.append('sort', sort);
+    }
+
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {
       params,
     });
