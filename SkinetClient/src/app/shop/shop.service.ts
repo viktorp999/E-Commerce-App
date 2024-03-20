868c43be7b3a0156af011b6cd59a9c1727ae3438
viktorp999 <viktorpetrovski99@gmail.com>
2024-03-20 02:26:38 +0100

    Searching in client side

--- SkinetClient/src/app/shop/shop.service.ts
+++ SkinetClient/src/app/shop/shop.service.ts
@@ -28,6 +28,10 @@ export class ShopService {
     params = params.append('pageIndex', shopParams.pageNumber);
     params = params.append('pageSize', shopParams.pageSize);
 
+    if (shopParams.search) {
+      params = params.append('search', shopParams.search);
+    }
+
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {
       params,
     });
