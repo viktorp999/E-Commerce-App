d50d47cfed9ceaef43866de7bc9aa61f5610e619
viktorp999 <viktorpetrovski99@gmail.com>
2024-03-22 00:22:09 +0100

    Lazy load shop module

--- SkinetClient/src/app/app.module.ts
+++ SkinetClient/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
-import { ShopModule } from './shop/shop.module';
 import { HomeModule } from './home/home.module';
 
 @NgModule({
@@ -16,7 +15,6 @@ import { HomeModule } from './home/home.module';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    ShopModule,
     HomeModule,
   ],
   providers: [],
